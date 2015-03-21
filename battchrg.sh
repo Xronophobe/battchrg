82b4d6fa624ee177f443c7ae023a18e8a530da58
Csanad <qed.csberes@gmail.com>
2015-03-21 15:08:20 +0100

    gitignore added

--- battchrg.sh
+++ battchrg.sh
@@ -6,17 +6,19 @@
 
 
 #storing system_profiler output:
-sysprofiler="$(system_profiler SPPowerDataType | grep -A4 'Charge Information')"
+sysprofiler="$(system_profiler SPPowerDataType|grep -A4 'Charge Information')"
 
 #current capacity in mAh:
-currcap=$(echo "${sysprofiler}" | grep 'Remaining' | awk '{print $4}')
+currcap=$(echo "${sysprofiler}"|grep 'Remaining'|awk '{print $4}')
 
 #full capacity in mAh:
-fullcap=$(echo "${sysprofiler}" | grep 'Full Charge' | awk '{print $5}')
+fullcap=$(echo "${sysprofiler}"|grep 'Full Charge'|awk '{print $5}')
 
 remaining=$(awk "BEGIN {printf \"%.1f\", 10*${currcap}/${fullcap}}")
-remaining_tens=$(echo $remaining | awk -F'.' '{print $1}')
-echo "remainig tenths are: $remaining_tens"
+remaining_tens=$(echo $remaining|awk -F'.' '{print $1}')
+remaining_ones=$(echo $remaining|awk -F'.' '{print $2}')
+      
+#echo "remainig tenths are: $remaining_tens"
 
 #if [[ remaining > 9.5 ]]; then
 #    remaining_tens='F'
@@ -25,7 +27,7 @@ echo "remainig tenths are: $remaining_tens"
 #    remaining_ones=$(echo $remaining | grep )
 
 bgcolor=$(tput sgr 7)
-echo $bgcolor$remaining
+echo $bgcolor$remaining_tens-$remaining_ones
 
 #echo $currcap
 echo $fullcap
